e62185bf30e6a0044fa99e5136675dcbb835248e
yilielin <98201330+yilielin@users.noreply.github.com>
2024-09-03 23:15:32 +0800

    Add AUTO_RUN_WINHOST flag in build_Emulator.bat

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Provide scripts and tools for EDK2 on Windows
 
 - src directory: source code location
-- tools: external tools location
+- tools: external tools for EDK2 (iasl and nasm)
 
 Directories ignored by git in workspace: 
 - Build
@@ -13,4 +13,5 @@ Directories ignored by git in workspace:
 1. Prepare essential environment of edk2 (Python3 and VS2022)
 1. Put edk2 and others package in src dir.
 1. Launch a cmd on workspace, or right ckick OpenCmdHere.bat.
-1. Excute build_xxx.bat to start platform compiling.
+1. Excute build_Emulator.bat to start platform compiling(EmulatorPkg).
+1. Set AUTO_RUN_WINHOST to 1 if you want run winhost in the end of build command
--- build_Emulator.bat
+++ build_Emulator.bat
@@ -8,10 +8,18 @@ set "TARGET=RELEASE"
 set "TARGET_ARCH=X64"
 set "TOOL_CHAIN_TAG=VS2022"
 
+:: auto run winhost, set to 1 to enable
+set "AUTO_RUN_WINHOST="
+
 call "src\edk2\edksetup.bat"
 
-call build -a %TARGET_ARCH% -t %TOOL_CHAIN_TAG% -b %TARGET% -p %ACTIVE_PLATFORM% 
+call build -a %TARGET_ARCH% -t %TOOL_CHAIN_TAG% -b %TARGET% -p %ACTIVE_PLATFORM%
 
+if "%AUTO_RUN_WINHOST%"=="1" goto :WIN_HOST
+:PROG_END
+goto :EOF
+
+:WIN_HOST
 :: Launch WinHost if it exist
 set "PATH_WINHOST=%WORKSPACE%\Build\Emulator%TARGET_ARCH%\%TARGET%_%TOOL_CHAIN_TAG%\%TARGET_ARCH%"
 if exist "%PATH_WINHOST%\WinHost.exe" (
@@ -19,3 +27,4 @@ if exist "%PATH_WINHOST%\WinHost.exe" (
     cmd /c WinHost.exe
     popd
 )
+goto :PROG_END
