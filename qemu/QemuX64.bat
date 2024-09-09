17b358a9cc8677908dfeb39ce006c1aa33f8365b
yilielin <98201330+yilielin@users.noreply.github.com>
2024-09-09 22:41:48 +0800

    Improve QemuX64.bat

--- qemu/QemuX64.bat
+++ qemu/QemuX64.bat
@@ -1,87 +1,71 @@
 @echo off
 chcp 65001 > NUL
-cd  %~d0%~p0 && %~d0
+set "SELF_PATH=%~d0%~p0"
 
-REM Run QEMU with disk as directory
-REM 1.set QEMU_MOUNT=DIR
-REM 2.Run QemuX64.bat 
+:: ==== CONFIG ====
+set "QEMU_OVMF_PATH=%SELF_PATH%\OVMF.fd"
+set "QEMU_EMULATOR=qemu-system-x86_64.exe"
+set "QEMU_MACHINE=q35"
+set "QEMU_SMP_CPUS=2"
+set "QEMU_MEM=512M"
+set "QEMU_NET=none"
+@REM set "QEMU_NET=rtl8139"
 
-REM Run QEMU with disk as VHDX
-REM 1.set QEMU_MOUNT=VHDX
-REM 2.Run QemuX64.bat with administer privilege
-
-REM QemuX64.bat m
-REM 1.mount vhdx
-
-REM QemuX64.bat u
-REM 1.unmount vhdx
-
-:: CONFIG
-set "QEMU_OVMF=%CD%\OVMF.fd"
-set "QEMU_X64=qemu-system-x86_64.exe"
-:: set mount option: VHDX or DIR
-set "QEMU_MOUNT=DIR"
-:: mounted VHDX
-set "VHDX_IMG=%CD%\disk0.vhdx"
-:: mounted DIR
-set "DISK_DIR=%CD%\disk0"
+:: QEMU_MOUNT_TYPE: NONE, VHDX or DIR
+set "QEMU_MOUNT_TYPE=DIR"
+set "QEMU_VHDX_PATH=%SELF_PATH%\disk0.vhdx"
+set "QEMU_DISK0_PATH=%SELF_PATH%\disk0"
 
 
-
-:: CHECK qemu and files
-%QEMU_X64% --help > NUL || ( echo " Can not invoke qemu-system-x86_64.exe " && goto :PROG_ERROR )
-if not exist %QEMU_OVMF% ( echo " OVMF is not found, exit. " && goto :PROG_ERROR )
-if not exist %VHDX_IMG% ( echo " VHDX is not found. " ) 
-if not exist %DISK_DIR% ( echo " DISK DIR is not found. " ) 
+:: ==== CHECK qemu and files ====
+%QEMU_EMULATOR% --help > NUL || ( echo " Can not invoke qemu-system-x86_64.exe " && goto :PROG_ERROR )
+if not exist %QEMU_OVMF_PATH% ( echo " OVMF is not found, exit. " && goto :PROG_ERROR )
+if not exist %QEMU_VHDX_PATH% ( echo " VHDX is not found. " ) 
+if not exist %QEMU_DISK0_PATH% ( echo " DISK DIR is not found. " ) 
 
 :: start
-
 if /I "%1"=="M" (
-    call :MOUNT_VHDX
+    call :MOUNT_QEMU_VHDX
     goto :END
 ) else if /I "%1"=="U" (
-    call :UNMOUNT_VHDX
+    call :UNMOUNT_QEMU_VHDX
     goto :END
 )
 
-
-echo Starting to run QEMU with OVMF
-echo.
-set "QEMU_EMULATOR= qemu-system-x86_64.exe -device usb-ehci -device usb-tablet -vga std "
-set "QEMU_MACHINE= -machine q35 "
-set "QEMU_SMP= -smp 2 "
-set "QEMU_MEM= -m 512M "
-set "QEMU_BIOS= -bios %QEMU_OVMF%"
-
-if "%QEMU_MOUNT%"=="VHDX" (
-  set "QEMU_DISK0= -drive format=vhdx,file=%VHDX_IMG% "
+if "%QEMU_MOUNT_TYPE%"=="VHDX" (
+  set "QEMU_DISK0_CMD= -drive format=vhdx,file=%QEMU_VHDX_PATH% "
   :: Test and unmount VHDX before QEMU starting up
-  call :UNMOUNT_VHDX
-)
-if "%QEMU_MOUNT%"=="DIR" (
-  set "QEMU_DISK0= -drive file=fat:rw:%DISK_DIR%,format=raw,media=disk "
+  call :UNMOUNT_QEMU_VHDX
+) else if "%QEMU_MOUNT_TYPE%"=="DIR" (
+  set "QEMU_DISK0_CMD= -drive file=fat:rw:%QEMU_DISK0_PATH%,format=raw,media=disk "
+) else (
+  set "QEMU_DISK0_CMD= "
 )
 
-:: QEMU without network
-set "QEMU_NET= -net none"
-:: QEMU with network
-REM set "QEMU_NET= -netdev user,id=net0 -device rtl8139,netdev=net0"
+
+:: Configure commands
+set "QEMU_EMULATOR_CMD= %QEMU_EMULATOR% -device usb-ehci -device usb-tablet -vga std "
+set "QEMU_MACHINE_CMD= -machine %QEMU_MACHINE% "
+set "QEMU_SMP_CMD= -smp %QEMU_SMP_CPUS% "
+set "QEMU_MEM_CMD= -m %QEMU_MEM% "
+set "QEMU_BIOS_CMD= -bios %QEMU_OVMF_PATH%"
+if "%QEMU_NET%"=="rtl8139" (
+  set "QEMU_NET_CMD= -netdev user,id=net0 -device rtl8139,netdev=net0 "
+) else (
+  set "QEMU_NET_CMD= -net none "
+)
 
 :: Print settings
-echo %QEMU_EMULATOR%
-echo %QEMU_MACHINE%
-echo %QEMU_BIOS%
-echo %QEMU_NET%
-echo %QEMU_DISK0%
-echo %QEMU_SMP% 
-echo %QEMU_MEM%
-:: Launch
-set "QEMU_UEFI_CMD=%QEMU_EMULATOR% %QEMU_MACHINE% %QEMU_SMP% %QEMU_MEM% %QEMU_NET% %QEMU_DISK0% %QEMU_BIOS%"
-echo Run %QEMU_UEFI_CMD%
+echo %QEMU_EMULATOR_CMD%
+echo %QEMU_MACHINE_CMD%
+echo %QEMU_BIOS_CMD%
+echo %QEMU_NET_CMD%
+echo %QEMU_DISK0_CMD%
+echo %QEMU_SMP_CMD% 
+echo %QEMU_MEM_CMD%
+:: Start to launch QEMU
+set "QEMU_UEFI_CMD=%QEMU_EMULATOR_CMD% %QEMU_MACHINE_CMD% %QEMU_SMP_CMD% %QEMU_MEM_CMD% %QEMU_NET_CMD% %QEMU_DISK0_CMD% %QEMU_BIOS_CMD%"
 %QEMU_UEFI_CMD%
-
-:: Uncomment the line as below to mount disk when QEMU is terminated
-REM call :MOUNT_VHDX
 
 :END
 goto :EOF
@@ -92,22 +76,25 @@ timeout 3
 goto :END
 
 
-:MOUNT_VHDX
-powershell -command "(Get-DiskImage -ImagePath "%VHDX_IMG%").Attached" > testMount.txt
+
+:MOUNT_QEMU_VHDX
+REM Mount VHDX need administer privilege
+powershell -command "(Get-DiskImage -ImagePath "%QEMU_VHDX_PATH%").Attached" > testMount.txt
 set /p isMount=<testMount.txt
 if /I "%isMount%"=="False" (
-    echo Mount %VHDX_IMG%
-    powershell -command "Mount-DiskImage -ImagePath "%VHDX_IMG%""  > NUL
+    echo Mount %QEMU_VHDX_PATH%
+    powershell -command "Mount-DiskImage -ImagePath "%QEMU_VHDX_PATH%""  > NUL
 )
 if exist testMount.txt ( del /Q testMount.txt )
 exit /b 0
 
-:UNMOUNT_VHDX
-powershell -command "(Get-DiskImage -ImagePath "%VHDX_IMG%").Attached" > testMount.txt
+:UNMOUNT_QEMU_VHDX
+REM Un-mount VHDX need administer privilege
+powershell -command "(Get-DiskImage -ImagePath "%QEMU_VHDX_PATH%").Attached" > testMount.txt
 set /p isMount=<testMount.txt
 if /I "%isMount%"=="True" (
-    echo Unmount %VHDX_IMG%
-    powershell -command "Dismount-DiskImage -ImagePath "%VHDX_IMG%"" > NUL
+    echo Unmount %QEMU_VHDX_PATH%
+    powershell -command "Dismount-DiskImage -ImagePath "%QEMU_VHDX_PATH%"" > NUL
 )
 if exist testMount.txt ( del /Q testMount.txt )
 exit /b 0
