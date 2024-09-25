7d68e3d1ebb38c018a7ead9bd2428a6e145c047b
yilielin <98201330+yilielin@users.noreply.github.com>
2024-09-25 22:32:34 +0800

    Set DEBUG build as default in build_OVMF

--- build_OVMF.bat
+++ build_OVMF.bat
@@ -5,7 +5,7 @@ call base_env_setup.bat
 set "EDK_TOOLS_PATH=%WORKSPACE%\src\edk2\BaseTools"
 set "PACKAGES_PATH=%WORKSPACE%\src\edk2"
 set "ACTIVE_PLATFORM=OvmfPkg\OvmfPkgX64.dsc"
-set "TARGET=RELEASE"
+set "TARGET=DEBUG"
 set "TARGET_ARCH=X64"
 set "TOOL_CHAIN_TAG=VS2022"
 
