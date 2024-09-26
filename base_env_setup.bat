07f5d6128697aa29cc426f4821ccbf3e818db6cb
yilielin <98201330+yilielin@users.noreply.github.com>
2024-09-26 22:10:26 +0800

    Update base_env_setup
    - Improve worksapce definition
    Use BUILD_WORKSPACE to store the path first,
    than check whether backslash is at the end of BUILD_WORKSPACE,
    remove backslash if it exist.

--- base_env_setup.bat
+++ base_env_setup.bat
@@ -3,7 +3,14 @@
 @ chcp 65001 > NUL
 
 :: Set the path of this file as workspace path 
-@ set "WORKSPACE=%~d0%~p0"
+@ set "BUILD_WORKSPACE=%~d0%~p0"
+
+:: Make sure there is no backslash at the end of WORKSPACE
+@ if "%BUILD_WORKSPACE:~-1%" EQU "\" (
+@   set "WORKSPACE=%BUILD_WORKSPACE:~0,-1%"
+) else (
+@   set "WORKSPACE=%BUILD_WORKSPACE%"
+)
 
 :: Setup NASM and IASL
 @ set "NASM_PREFIX=%WORKSPACE%\tools\nasm-2.16.03\"
